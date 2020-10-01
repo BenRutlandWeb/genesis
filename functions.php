26b208ebd89bbeafb72cee6408fd2f30fd8d3cb2
Ben Rutland <benrutland@hotmail.com>
2020-10-01 17:14:43 +0100

    added test command

--- functions.php
+++ functions.php
@@ -5,3 +5,16 @@ require __DIR__ . '/vendor/autoload.php';
 $app = new Genesis\Foundation\Application(__DIR__);
 
 $app->bootstrap();
+
+if (class_exists('WP_CLI')) {
+    Console::command('tell a joke', function () {
+
+        $response = wp_remote_get('https://official-joke-api.appspot.com/jokes/programming/random');
+
+        $json = json_decode(wp_remote_retrieve_body($response));
+
+        $this->ask($json->setup);
+
+        $this->line($json->punchline);
+    });
+}
