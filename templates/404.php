58eff92db7cef9608c537361ef36375efbed9364
Ben Rutland <ben@10degrees.uk>
2020-10-01 14:34:23 +0100

    added 404 template

---
+++ templates/404.php
@@ -0,0 +1,15 @@
+<?php echo view('partials.header'); ?>
+
+    <div class="container">
+        <div class="prose mx-auto my-16">
+            <h1><?php _e('404: Page not found', 'genesis'); ?></h1>
+            <p><?php _e('Awwww shucks', 'genesis'); ?></p>
+            <p>
+                <a href="<?php echo url()->home(); ?>">
+                    <?php _e('Go back home', 'genesis'); ?>
+                </a>
+            </p>
+        </div>
+    </div>
+
+<?php echo view('partials.footer'); ?>
