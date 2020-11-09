ad9f120428718681b7ece10b7825278253e19aa4
Ben Rutland <benrutland@hotmail.com>
2020-11-09 00:15:47 +0000

    removed blade compiled views

--- storage/48e08223334e306d138196b1f6729e5b6fa81379.php
+++
@@ -1,6 +0,0 @@
-<h1><?php echo e(the_title()); ?></h1>
-
-<?php echo e(the_content()); ?>
-
-
-<p><?php echo e(the_permalink()); ?></p><?php /**PATH /app/public/wp-content/themes/genesis/resources/views/partials/content.blade.php ENDPATH**/ ?>
--- storage/fd8394613d67a3bb0d30edfd83876e9401d4998a.php
+++
@@ -1,4 +0,0 @@
-<div class="p-4 my-4 bg-<?php echo e($type); ?> text-white rounded">
-    <?php echo e($message); ?>
-
-</div><?php /**PATH /app/public/wp-content/themes/genesis/resources/views/components/alert.blade.php ENDPATH**/ ?>
